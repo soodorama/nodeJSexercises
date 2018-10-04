596c20ee318594094d39effe571dceb368ab3db5
Neil Sood <soodorama@gmail.com>
2018-10-04 15:27:56 -0700

    cars and cats

---
+++ cars_and_cats/app.js
@@ -0,0 +1,61 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/cats') {
+        fs.readFile('cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/cars/new') {
+        fs.readFile('cars_new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/stylesheets/style.css') {
+        fs.readFile('./stylesheets/style.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/images/bugatti.jpg') {
+        fs.readFile('./images/bugatti.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/images/cat.jpg') {
+        fs.readFile('./images/cat.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// print to terminal window
+console.log("Running in localhost at port 6789");
